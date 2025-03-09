76db79e13faafc88a4da944d6ab3d4cb3d6fd26d
Arm1700 <bukboks1@gmail.com>
2025-03-09 14:56:41 +0400

    setting

--- school/settings.py
+++ school/settings.py
@@ -202,7 +202,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
